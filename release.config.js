ba99122b01b951f65723a946ce16b5dbe6eda75a
PleBea <bini060315kim@gmail.com>
2023-10-25 00:58:15 +0900

    change release config

--- release.config.js
+++ release.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        tarballDir: 'release',
-      },
-    ],
     [
       '@semantic-release/github',
       {
